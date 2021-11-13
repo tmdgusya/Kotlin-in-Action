267253ebc9dffc62351276ccae63c9f17c20d9aa
roach <tmdgusya@naver.com>
2021-11-13 23:54:30 +0900

    Person Getter Setter Default Constructor

--- src/Example01/HelloWorld.kt
+++ src/Example01/HelloWorld.kt
@@ -2,5 +2,9 @@ package Example01
 
 fun main(args: Array<String>) {
     val name: String = if (args.isNotEmpty()) args[0] else "Kotlin"
+
+    var person: Person = Person("roach", false);
+    person = Person("New Person", true);
+
     println("Hello $name")
 }
