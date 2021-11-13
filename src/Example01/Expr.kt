50995bd7df850be5462d8910183c01c7cf9f7841
roach <tmdgusya@naver.com>
2021-11-14 00:28:50 +0900

    when 으로 refactoring

--- src/Example01/Expr.kt
+++ src/Example01/Expr.kt
@@ -10,13 +10,12 @@ interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
-fun eval(e: Expr): Int {
-    if (e is Num) {
-        val n: Num = e as Num;
-        return n.value;
+fun eval(e: Expr): Int =
+    when (e) {
+        is Num ->
+            e.value
+        is Sum ->
+            eval(e.right) + eval(e.left);
+        else ->
+            throw IllegalArgumentException("Unknown Expression");
     }
-    if (e is Sum) {
-        return eval(e.right) + eval(e.left);
-    }
-    throw IllegalArgumentException("Unknown Expression");
-}
