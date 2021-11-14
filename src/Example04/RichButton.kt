73cc4902ef7adad3cd20614ea42653b106b63f51
roach <tmdgusya@naver.com>
2021-11-14 12:00:16 +0900

    open final override

---
+++ src/Example04/RichButton.kt
@@ -0,0 +1,11 @@
+package Example04
+
+open class RichButton : Clickable {
+
+    fun disable() {}
+
+    open fun animate() {}
+
+    override fun click() {}
+
+}
---
+++ src/Example04/RichButtonChild.kt
@@ -0,0 +1,12 @@
+package Example04
+
+class RichButtonChild : RichButton() {
+
+    override fun animate() {
+        super.animate()
+    }
+
+    override fun click() {
+        super.click()
+    }
+}
