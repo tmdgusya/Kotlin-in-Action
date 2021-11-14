66e4e8205cc50c6d2d10f5d570c4b60c97be08f2
roach <tmdgusya@naver.com>
2021-11-14 12:32:08 +0900

    primary constructor

--- src/Example04/User.kt
+++ src/Example04/User.kt
@@ -1,9 +1,17 @@
 package Example04
 
 class User constructor (nickname: String) {
-    private val nickname: String
+    val nickname: String;
+    val isSubscribed: Boolean = true;
 
     init {
         this.nickname = nickname
     }
+}
+
+fun main(args: Array<String>) {
+    val user: User = User("Roach");
+
+    println(user.nickname)
+    println(user.isSubscribed)
 }
