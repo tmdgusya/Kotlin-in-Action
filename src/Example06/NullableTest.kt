0f2793040a9d471071faac9ec22fd410ac841d15
roach <tmdgusya@naver.com>
2021-11-18 19:05:49 +0900

    safety nullable

--- src/Example06/NullableTest.kt
+++ src/Example06/NullableTest.kt
@@ -4,11 +4,8 @@ import kotlin.IllegalArgumentException
 
 class NullableTest() {}
 
-fun strLen(s: String?): Int {
-    if (s == null) {
-        throw IllegalArgumentException("잘못된 입력값 입니다.");
-    }
-    return s.length
+fun strLen(s: String?): Int? {
+    return s?.length
 };
 
 fun main(args: Array<String>) {
